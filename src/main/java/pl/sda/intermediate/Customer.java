7bb85bec4376be11043a2c6fd960ec3c78533b4a
Krzysztof Pawelec <klopr@interia.pl>
2019-02-09 20:39:02 +0100

    Customer

---
+++ src/main/java/pl/sda/intermediate/Customer.java
@@ -0,0 +1,25 @@
+package pl.sda.intermediate;
+
+import java.math.BigDecimal;
+
+public class Customer {
+
+    private Integer id;
+    private String name;
+    private String surname;
+    private Integer age;
+    private BigDecimal salary;
+    private static Integer COUNTER;
+
+    public Customer(String name, String surname, Integer age, Integer salary) {
+        this.id = COUNTER+1;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.salary = BigDecimal.valueOf(salary);
+    }
+
+    public Customer(String name, String surname, Integer age, String salary) {
+        this (name,surname,age,Integer.valueOf(salary));
+    }
+}
