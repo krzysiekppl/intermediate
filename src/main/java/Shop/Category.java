9abae04c736a68736e8530eae58c64c112f639ab
Krzysztof Pawelec <klopr@interia.pl>
2019-02-10 14:31:25 +0100

    File loader

---
+++ src/main/java/Shop/Category.java
@@ -0,0 +1,16 @@
+package Shop;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+public class Category {
+
+    private Integer id;
+    private Integer parentId;
+    private String name;
+}
---
+++ src/main/java/Shop/InMemoryCategoryDAO.java
@@ -0,0 +1,51 @@
+package Shop;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class InMemoryCategoryDAO {
+    List<Category> categoryListResult = new ArrayList<>();
+
+    private void initializeCategories() {
+        List<String> lines = loadCategoriesFromFile();
+        List<Category> categoryList = new ArrayList<>();
+
+        for (int i = 1; i <= lines.size(); ++i) {
+            categoryList.add(Category
+                    .builder()
+                    .id(i)
+                    .name(lines.get(i-1))
+                    .build());
+        }
+        Map<Integer,List<Category>> categoriesMap = new HashMap<>();
+        for (Category category : categoryList){
+            String[] split = category.getName().split("\\S");
+            int depth = split[0].length();
+            if (categoriesMap.containsKey(depth)){
+                categoriesMap.get(depth).add(category);
+            }else {
+                List<Category> innerList = new ArrayList<>();
+                innerList.add(category);
+                categoriesMap.put(depth,innerList);
+            }
+        }
+
+    }
+
+    private List<String> loadCategoriesFromFile() {
+
+        ClassLoader loader = this.getClass().getClassLoader();
+        try {
+            Path path = Paths.get(loader.getResource("kategorie.txt").toURI());
+            return Files.readAllLines(path);
+
+        } catch (URISyntaxException | IOException e) {
+            e.printStackTrace();
+        }
+        return Collections.emptyList();
+    }
+}
